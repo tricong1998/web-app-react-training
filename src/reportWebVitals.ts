ca6ae77a2d7923fcd96d17d1df069a67a7899e87
Conan <conan.congnguyen@setel.com>
2023-01-04 18:12:42 +0700

    fix: eslint rule

--- src/reportWebVitals.ts
+++ src/reportWebVitals.ts
@@ -1,14 +1,13 @@
 import { ReportHandler } from 'web-vitals';
 
-const reportWebVitals = (onPerfEntry?: ReportHandler) => {
-  if (onPerfEntry && onPerfEntry instanceof Function) {
-    import('web-vitals').then(({ getCLS, getFID, getFCP, getLCP, getTTFB }) => {
-      getCLS(onPerfEntry);
-      getFID(onPerfEntry);
-      getFCP(onPerfEntry);
-      getLCP(onPerfEntry);
-      getTTFB(onPerfEntry);
-    });
+const reportWebVitals = async (onPerfEntry?: ReportHandler) => {
+  if (onPerfEntry != null && onPerfEntry instanceof Function) {
+    const { getCLS, getFID, getFCP, getLCP, getTTFB } = await import('web-vitals');
+    getCLS(onPerfEntry);
+    getFID(onPerfEntry);
+    getFCP(onPerfEntry);
+    getLCP(onPerfEntry);
+    getTTFB(onPerfEntry);
   }
 };
 
