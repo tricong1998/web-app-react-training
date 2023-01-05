db21582925c7e8a263a8eec00b476c749e013d24
Conan <conan.congnguyen@setel.com>
2023-01-05 11:41:38 +0700

    fix: tailwind config

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,8 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: [],
+  content: [
+    './src/**/*.{js,jsx,ts,tsx}',
+  ],
   presets: [],
   darkMode: 'media', // or 'class'
   theme: {
@@ -967,5 +969,5 @@ module.exports = {
     'active',
     'disabled',
   ],
-  plugins: [],
+  plugins: []
 }
